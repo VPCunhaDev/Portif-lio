2e02765a37e8bbc44ae56b7dcd6fcf87c11457fe
VPCunhaDev <viniciuscunhadev@outlook.com>
2026-04-21 19:54:36 -0300

    Acesso ao BD pelos controllers

--- ApiCatalogo/obj/Debug/net10.0/ApiCatalogo.AssemblyInfo.cs
+++ ApiCatalogo/obj/Debug/net10.0/ApiCatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d2c0927d20f0e910eb0bb58dfecf3d2fd398c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
